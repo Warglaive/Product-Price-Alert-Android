c40b165612fdd03859dfba8311fe1cdb9945178c
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-09 15:34:35 +0200

    add

--- Implementation/ProductPriceAlert/app/src/main/java/com/activities/MainActivity.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.productpricealert;
+package com.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.productpricealert.R;
 
 public class MainActivity extends AppCompatActivity {
 
