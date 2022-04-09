2ac098ce7c7cf0291a2db9894bed4847a74d6340
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-09 17:02:03 +0200

    retrofit up/change min sdk to 24

--- Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/Controller.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.vogella.retrofitgerrit.Interfaces.GerritAPI;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,14 +13,32 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Controller implements Callback<List<Change>> {
-    @Override
-    public void onResponse(Call<List<Change>> call, Response<List<Change>> response) {
+    //TODO: Fix BaseURl
+    static final String baseUrl = "localhost:3000";
+
+    public void start() {
+        Gson gson = new GsonBuilder().setLenient().create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        GerritAPI gerritAPI = retrofit.create(GerritAPI.class);
+        Call<List<Change>> call = gerritAPI.loadChanges("status:open");
+        call.enqueue(this);
 
     }
 
     @Override
-    public void onFailure(Call<List<Change>> call, Throwable t) {
+    public void onResponse(Call<List<Change>> call, Response<List<Change>> response) {
+        if (response.isSuccessful()) {
+            List<Change> changesList = response.body();
+            changesList.forEach(c -> System.out.println(c.subject));
+        }
+    }
 
+    @Override
+    public void onFailure(Call<List<Change>> call, Throwable t) {
+        t.printStackTrace();
     }
 }
 
