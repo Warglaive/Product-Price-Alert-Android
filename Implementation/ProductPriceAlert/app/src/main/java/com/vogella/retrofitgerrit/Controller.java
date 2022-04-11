d01f6ec265672c7feb86d02b3ae7ffbe2b63ae50
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-11 14:35:49 +0200

    Update Controller.java

--- Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/Controller.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/Controller.java
@@ -14,7 +14,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Controller implements Callback<List<UserData>> {
-    //TODO: Fix BaseURl
     static final String baseUrl = "http://127.0.0.1:3000/";
     Retrofit retrofit;
     GerritAPI gerritAPI;
