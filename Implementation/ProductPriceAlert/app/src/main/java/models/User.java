6c8da9126324c53ceb436fe740c5cfd8bb6e3503
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-21 14:00:48 +0200

    Update User.java

--- Implementation/ProductPriceAlert/app/src/main/java/models/User.java
+++ Implementation/ProductPriceAlert/app/src/main/java/models/User.java
@@ -2,11 +2,15 @@ package models;
 
 public class User {
     private String name;
-    private int id;
+    private String email;
+    private String password;
+    /*private int id;*/
     private String type;
 
-    public User(String name, String type) {
+    public User(String name, String email, String password, String type) {
         this.name = name;
+        this.email = email;
+        this.password = password;
         this.type = type;
     }
 }
