c4a13216f9ff05b337b174c80c65b5c350f1ba99
MonikaDobreva <m.dobreva@student.fontys.nl>
2022-04-24 20:04:59 +0200

    Create Product.java

---
+++ Implementation/ProductPriceAlert/app/src/main/java/com/models/Product.java
@@ -0,0 +1,38 @@
+package com.models;
+
+
+import android.media.Image;
+
+public class Product {
+    String name;
+    double price;
+    private Image image;
+    private String description;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name, double price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public Image getImage() {
+        return this.image;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+}
