6de9b5a64aa4b8d64d10dc5647ac1bd67a6987dd
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-24 21:40:59 +0200

    Update RestAPI.java

--- Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/interfaces/RestAPI.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/interfaces/RestAPI.java
@@ -6,15 +6,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface RestAPI {
     @GET("http://192.168.0.116:3000/users/")
     Call<List<UserData>> getAllUsers();
-
-    //TODO: Post
 
     @POST("http://192.168.0.116:3000/users/")
     Call<UserData> postUser(@Body UserData userData);
