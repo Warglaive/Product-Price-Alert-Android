7d92c3bb39e8361901320fffc9aee21f2526fae6
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-29 20:44:10 +0200

    Revert "test"
    This reverts commit b487bde7f3eba85d094162d27f0a2b710fd4174c.

--- prj-4-group-android-group3/src/application.ts
+++ prj-4-group-android-group3/src/application.ts
@@ -1,7 +1,17 @@
+//Authentication
+import { AuthenticationComponent } from '@loopback/authentication';
+import {
+  JWTAuthenticationComponent,
+  SECURITY_SCHEME_SPEC,
+  UserServiceBindings,
+} from '@loopback/authentication-jwt';
+import { MongoDataSource } from './datasources';
+// ------------------------------------
 
-import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
 
+//
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig } from '@loopback/core';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
@@ -42,5 +52,13 @@ export class Prj4GroupAndroidGroup3Application extends BootMixin(
         nested: true,
       },
     };
+    // ------ ADD SNIPPET AT THE BOTTOM ---------
+    // Mount authentication system
+    this.component(AuthenticationComponent);
+    // Mount jwt component
+    this.component(JWTAuthenticationComponent);
+    // Bind datasource
+    this.dataSource(MongoDataSource, UserServiceBindings.DATASOURCE_NAME);
+    // ------------- END OF SNIPPET -------------
   }
 }
