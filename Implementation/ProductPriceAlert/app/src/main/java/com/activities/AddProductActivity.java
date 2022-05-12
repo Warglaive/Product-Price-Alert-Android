b2808e386bf7907b12f0b7855b936c018468cf01
phuynh0211 <60883519+phuynh0211@users.noreply.github.com>
2022-05-12 13:18:17 +0200

    Update AddProductActivity.java

--- Implementation/ProductPriceAlert/app/src/main/java/com/activities/AddProductActivity.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/AddProductActivity.java
@@ -64,101 +64,14 @@ public class AddProductActivity extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(AddProductActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
 
-        //TODO ask for permission of camera upon first launch of application
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED) {
-                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                requestPermissions(permission, 111);
-            }
-        }
 
-        this.homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(AddProductActivity.this, HomePageActivity.class));
-            }
-        });
-
-        //TODO chose image from gallery
-        addGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-
-            }
-        });
-
-        //TODO capture image
-        takePhotoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_DENIED) {
-                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        requestPermissions(permission, 111);
-                    } else {
-                        openCamera();
-                    }
-                } else {
-                    openCamera();
-                }
-            }
-        });
 }
 
-    Uri image_uri;
-    private static final int RESULT_LOAD_IMAGE = 123;
-    public static final int IMAGE_CAPTURE_CODE = 654;
-    //TODO opens camera so that user can capture image
-    private void openCamera() {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == IMAGE_CAPTURE_CODE && resultCode == RESULT_OK){
-            //imageView.setImageURI(image_uri);
-            Bitmap bitmap = uriToBitmap(image_uri);
-            imageView.setImageBitmap(bitmap);
-
-        }
 
 
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
-            image_uri = data.getData();
-            //imageView.setImageURI(image_uri);
-            Bitmap bitmap = uriToBitmap(image_uri);
-            imageView.setImageBitmap(bitmap);
 
-        }
-    }
 
-    //TODO takes URI of the image and returns bitmap
-    private Bitmap uriToBitmap(Uri selectedFileUri) {
-        try {
-            ParcelFileDescriptor parcelFileDescriptor =
-                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
-            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
 
-            parcelFileDescriptor.close();
-            return image;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return  null;
-    }
 
 
 
