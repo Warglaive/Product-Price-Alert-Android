303000103f69220f7a141ea47ab5d934134df904
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-12 14:50:18 +0200

    Update user.repository.ts

--- prj-4-group-android-group3/src/repositories/user.repository.ts
+++ prj-4-group-android-group3/src/repositories/user.repository.ts
@@ -1,6 +1,5 @@
 import { inject } from '@loopback/core';
 import { DefaultCrudRepository } from '@loopback/repository';
-import { use } from 'should';
 import { MongoDataSource } from '../datasources';
 import { User, UserRelations } from '../models';
 
