7c5d66c0c6a05afae7fbfbbb56c865ab1be75728
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-13 22:50:38 +0200

    activity_product_manager

---
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/ProductManagerActivity.java
@@ -0,0 +1,17 @@
+package com.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.productpricealert.R;
+
+public class ProductManagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
