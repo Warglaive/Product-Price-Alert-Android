70c449e42b01771b48bbfe41355071fca76d7565
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-13 19:47:21 +0200

    user.repository.ts
    custom login

--- prj-4-group-android-group3/src/repositories/user.repository.ts
+++ prj-4-group-android-group3/src/repositories/user.repository.ts
@@ -3,17 +3,18 @@ import { DefaultCrudRepository } from '@loopback/repository';
 import { MongoDataSource } from '../datasources';
 import { User, UserRelations } from '../models';
 
-export class UserRepository extends DefaultCrudRepository< User,  typeof User.prototype.id,  UserRelations> {
+export class UserRepository extends DefaultCrudRepository<User, typeof User.prototype.id, UserRelations> {
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
   ) {
     super(User, dataSource);
   }
   //Find by Email custom method
-  findByEmail(email: string): User | PromiseLike<User> {
+  login(email: string, password: string): User | PromiseLike<User> {
     const user = this.findOne({
       where: {
-        email: email
+        email: email,
+        password: password
       }
     }) as unknown as User
     return user;
