6a8b36d48c355853f06c7f968abf1cae3be40b26
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-14 21:26:16 +0200

    ProductManagerActivity

--- Implementation/ProductPriceAlert/app/src/main/java/com/activities/ProductManagerActivity.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/ProductManagerActivity.java
@@ -1,5 +1,6 @@
 package com.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,10 +30,29 @@ public class ProductManagerActivity extends AppCompatActivity {
         Gson gson = new Gson();
         UserData userData = gson.fromJson(getIntent().getStringExtra(UserDataKey), UserData.class);
         //Find fields
+        findFieldsById();
+        //set fields initial values
+        DisplayUserData(userData);
+        //
+       
+    }
+
+    private void DisplayUserData(UserData userData) {
+        String welcomeMessage = "Welcome " + userData.getName();
+        this.welcomeNamePM.setText(welcomeMessage);
+        this.welcomeNamePM.setTextColor(Color.parseColor("#2596be"));
+        //
+        String roleDisplay = "Your Role is: " + userData.getRole();
+        this.showRolePM.setText(roleDisplay);
+        this.showRolePM.setTextColor(Color.parseColor("#2596be"));
+    }
+
+    private void findFieldsById() {
         this.welcomeNamePM = findViewById(R.id.welcomeNamePM);
         this.showRolePM = findViewById(R.id.showRolePM);
         this.browseProductsPM = findViewById(R.id.browseProductsPM);
         this.addNewPRBtn = findViewById(R.id.addNewPRBtn);
         this.logoutPM = findViewById(R.id.logoutPM);
     }
+
 }
