f690952dce32e3750eee54144643962208b1338d
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-22 19:25:53 +0200

    PushNotificationService

--- Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
@@ -1,12 +1,15 @@
 package com.services;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.productpricealert.R;
 
 public class PushNotificationService extends FirebaseMessagingService {
     @Override
@@ -16,7 +19,20 @@ public class PushNotificationService extends FirebaseMessagingService {
         final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
         final String channelName = "Heads up notification";
 
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH);
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        //Create a notification
+        Notification.Builder notification =
+                new Notification.Builder(this, CHANNEL_ID)
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setAutoCancel(false);
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+        //
         super.onMessageReceived(message);
     }
 }
