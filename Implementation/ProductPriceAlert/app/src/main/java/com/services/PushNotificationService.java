41d168de9a5cf481abf2540835370cb2ab17a5a8
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-22 18:56:27 +0200

    minSdk 26
    PushNotificationService

--- Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
@@ -1,6 +1,22 @@
 package com.services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+import androidx.annotation.NonNull;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class PushNotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        String title = message.getNotification().getTitle();
+        String text = message.getNotification().getBody();
+        final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
+        final String channelName = "Heads up notification";
+
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        super.onMessageReceived(message);
+    }
 }
