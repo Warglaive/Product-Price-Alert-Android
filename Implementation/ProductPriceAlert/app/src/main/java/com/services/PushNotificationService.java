546b254f2ecdda90311980ee4085fe0cc9e3c7dd
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-26 21:40:22 +0200

    PushNotificationService

--- Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
@@ -3,10 +3,16 @@ package com.services;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.activities.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.ProductPriceAlert.R;
@@ -15,6 +21,37 @@ import java.util.Objects;
 
 
 public class PushNotificationService extends FirebaseMessagingService {
+    private static final String TAG = MainActivity.class.getName();
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast //IT WORKS, FUCK INTELLIJ, Just ignore the error...
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        //Needed activity
+                        MainActivity testActivity = new MainActivity();
+                        Toast.makeText(testActivity.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    /**
+     * Display on foreground (not background)
+     *
+     * @param message
+     */
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         String title = Objects.requireNonNull(message.getNotification()).getTitle();
