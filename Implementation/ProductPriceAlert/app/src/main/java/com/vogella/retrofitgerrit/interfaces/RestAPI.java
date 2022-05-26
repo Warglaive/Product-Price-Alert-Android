48c1238788324bf0df19e9d15c5748ac182325d3
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-26 20:24:57 +0200

    firebase/app also FCM

--- Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/interfaces/RestAPI.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/vogella/retrofitgerrit/interfaces/RestAPI.java
@@ -13,24 +13,28 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RestAPI {
-    @GET("http://192.168.0.116:3000/users/")
+    @GET("http://172.28.176.1:3000/users/")
     Call<List<UserData>> getAllUsers();
 
-    @POST("http://192.168.0.116:3000/users/")
+    @POST("http://172.28.176.1:3000/users/")
     Call<UserData> postUser(@Body UserData userData);
 
-    @GET("http://192.168.0.116:3000/products/")
+    @GET("http://172.28.176.1:3000/products/")
     Call<List<ProductData>> getAllProducts();
 
-    @POST("http://192.168.0.116:3000/products/")
+    @POST("http://172.28.176.1:3000/products/")
     Call<ProductData> postProduct(@Body ProductData productData);
 
-    @GET("http://192.168.0.116:3000/products/searchBy/{searchTerm}")
+    @GET("http://172.28.176.1:3000/products/searchBy/{searchTerm}")
     Call<List<ProductData>> getFilteredProducts(@Path("searchTerm") String searchTerm);
 
-    @GET("http://192.168.0.116:3000/users/login/{email}")
+    @GET("http://172.28.176.1:3000/users/login/{email}")
     Call<UserData> findByEmail(@Path("email") String email);
 
-    @PATCH("http://192.168.0.116:3000/products/{id}")
+    @PATCH("http://172.28.176.1:3000/products/{id}")
     Call<ProductData> updatePrice(@Body String id);
+    //Cloud messaging
+  //  @Headers("Authorization : key=AAAA4Ubio1Q:APA91bGWkw84b1XX2nnnOKn8MO25U2giLRXXXTUkXidojFluZk_qKXXXlS27oMZZV5goTQdwRtpdmvI1iAPRZZDNKz6c-mpU6nvHZJ-Jg9f1fQ5NdttftqUpqwAkObLEEX26VFDDbXN8")
+  //  @POST("fcm/send")
+  //  Call<ResponseBody> sendChatNotification(@Field("to") String token, @Body RequestNotificaton requestNotificaton);
 }
