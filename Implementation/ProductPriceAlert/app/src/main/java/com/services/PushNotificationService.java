0a14525060b5f2f60c9412996e6c52437821eb7c
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-05-26 22:08:29 +0200

    strings.xml

--- Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/services/PushNotificationService.java
@@ -3,10 +3,17 @@ package com.services;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.activities.MainActivity;
@@ -21,34 +28,34 @@ import java.util.Objects;
 
 
 public class PushNotificationService extends FirebaseMessagingService {
-    private static final String TAG = MainActivity.class.getName();
+    private static final String TAG = "MyFirebaseMsgService";
 
+    /**
+     * There are two scenarios when onNewToken is called:
+     * 1) When a new token is generated on initial app startup
+     * 2) Whenever an existing token is changed
+     * Under #2, there are three scenarios when the existing token is changed:
+     * A) App is restored to a new device
+     * B) User uninstalls/reinstalls the app
+     * C) User clears app data
+     */
     @Override
     public void onNewToken(@NonNull String token) {
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
+        Log.d(TAG, "Refreshed token: " + token);
 
-                        // Get new FCM registration token
-                        String token = task.getResult();
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token);
+    }
 
-                        // Log and toast //IT WORKS, FUCK INTELLIJ, Just ignore the error...
-                        String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d(TAG, msg);
-                        //Needed activity
-                        MainActivity testActivity = new MainActivity();
-                        Toast.makeText(testActivity.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+
     }
 
     /**
-     * Display on foreground (not background)
+     * Display notification on foreground
      *
      * @param message
      */
@@ -74,5 +81,41 @@ public class PushNotificationService extends FirebaseMessagingService {
         NotificationManagerCompat.from(this).notify(1, notification.build());
         //
         super.onMessageReceived(message);
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setContentTitle(getString(R.string.fcm_message))
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
