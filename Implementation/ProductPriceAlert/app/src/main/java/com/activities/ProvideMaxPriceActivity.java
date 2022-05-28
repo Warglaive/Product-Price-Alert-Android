47bdcf092af39fba7b881d4d378e3c748fc7e931
MonikaDobreva <m.dobreva@student.fontys.nl>
2022-05-28 23:36:04 +0200

    max price in progress

---
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/ProvideMaxPriceActivity.java
@@ -0,0 +1,16 @@
+package com.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ProductPriceAlert.R;
+
+public class ProvideMaxPriceActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provide_maximum_price);
+    }
+}
