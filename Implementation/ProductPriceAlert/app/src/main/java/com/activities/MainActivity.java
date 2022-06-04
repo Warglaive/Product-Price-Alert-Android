aba232f83841ca3ae9e1fdc3e52e67683ba0a44d
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-06-04 17:27:01 +0200

    added test case to main (WONT AFFECT OTHER THINGS)

--- Implementation/ProductPriceAlert/app/src/main/java/com/activities/MainActivity.java
+++ Implementation/ProductPriceAlert/app/src/main/java/com/activities/MainActivity.java
@@ -1,20 +1,30 @@
 package com.activities;
 
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ProductPriceAlert.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.services.PushNotificationService;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button registerButton;
     private Button loginButton;
+    //Notification test
+    private Button notifyMe;
 
 
     @Override
@@ -24,11 +34,68 @@ public class MainActivity extends AppCompatActivity {
         //Initialize XML elements
         this.registerButton = findViewById(R.id.registerButton);
         this.loginButton = findViewById(R.id.loginButton);
-
+        this.notifyMe = findViewById(R.id.notifyMe);
         loginOrRegister(findViewById(R.id.mainActivity).getRootView());
         //TODO: Test notifications
-
+        NotificationOnClickListenerSet();
     }
+
+    private void NotificationOnClickListenerSet() {
+        this.notifyMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Subscribing to weather topic");
+                // [START subscribe_topics]
+                FirebaseMessaging.getInstance().subscribeToTopic("weather")
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                String msg = getString(R.string.msg_subscribed);
+                                if (!task.isSuccessful()) {
+                                    msg = getString(R.string.msg_subscribe_failed);
+                                }
+                                Log.d(TAG, msg);
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                // [END subscribe_topics]
+            }
+        });
+
+        /**
+         * Log Token
+         */
+
+        this.notifyMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get token
+                // [START log_reg_token]
+                FirebaseMessaging.getInstance().getToken()
+                        .addOnCompleteListener(new OnCompleteListener<String>() {
+                            @Override
+                            public void onComplete(@NonNull Task<String> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                                    return;
+                                }
+
+                                // Get new FCM registration token
+                                String token = task.getResult();
+
+                                // Log and toast
+                                //TODO: Ignore the warning
+                                String msg = getString(R.string.msg_token_fmt, token);
+                                Log.d(TAG, msg);
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                // [END log_reg_token]
+            }
+        });
+    }
+
+
 
     /**
      * Called when the user taps the Send button
