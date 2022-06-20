174161109f4483741e25bcd45567aa4ef38ce2d1
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-06-20 15:02:58 +0200

    removed useless Lb4 leftovers

--- prj-4-group-android-group3/dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { Prj4GroupAndroidGroup3Application } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: Prj4GroupAndroidGroup3Application;
-    client: Client;
-}
--- prj-4-group-android-group3/dist/repositories/product.repository.d.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { MongoDataSource } from '../datasources';
-import { Product, ProductRelations } from '../models';
-export declare class ProductRepository extends DefaultCrudRepository<Product, typeof Product.prototype.id, ProductRelations> {
-    constructor(dataSource: MongoDataSource);
-    findBySearchTerm(searchTerm: string): Product | PromiseLike<Product>;
-}
